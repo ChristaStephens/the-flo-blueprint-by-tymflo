5b44dcc624532facb02c16d1dc7cec956ed30db1
Christa Stephens <43422147+ChristaStephens@users.noreply.github.com>
2026-07-12 17:08:22 -0400

    Add replit.nix

---
+++ replit.nix
@@ -0,0 +1,30 @@
+{pkgs}: {
+  deps = [
+    pkgs.chromium
+    pkgs.libGL
+    pkgs.at-spi2-core
+    pkgs.xorg.libXtst
+    pkgs.xorg.libXrender
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXext
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libX11
+    pkgs.dbus
+    pkgs.alsa-lib
+    pkgs.cairo
+    pkgs.pango
+    pkgs.mesa
+    pkgs.libxkbcommon
+    pkgs.xorg.libxcb
+    pkgs.libdrm
+    pkgs.expat
+    pkgs.cups
+    pkgs.at-spi2-atk
+    pkgs.atk
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
+  ];
+}
